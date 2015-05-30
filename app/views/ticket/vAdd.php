8acf692668810ef3de566a6d6ed4a6f4f391af56
AntoineIcleanu <a.icleanu@laposte.net>
2015-05-30 13:35:25 +0200

    Problème ticket
    Réglage d'un problème ou la moitié de page pour la création de ticket
    n'apparaissais pas
    Problème résolue mais avec un petit message d'erreur dans un des champs

--- app/views/ticket/vAdd.php
+++ app/views/ticket/vAdd.php
@@ -1,6 +1,6 @@
 <form method="post" action="Tickets/add">
     <fieldset>
-        <legend>Creer un ticket</legend>
+        <legend>Creation d'un ticket</legend>
         <div class="form-group">
             <select name="type" class="form-control">
                 <option disabled selected>Type</option>
@@ -9,35 +9,35 @@
                 <option value="<?php echo $type; ?>"><?php echo $libelle; ?></option>
 <?php } ?>
             </select>
-            <br />
+            <br/>
             <select name="categorie" class="form-control">
-                <option disabled selected>Catégorie</option>
+                <option disabled selected>Categorie</option>
                 <option disabled>---</option>
 <?php foreach($categories as $cat) {  ?>
                     <option value="<?php echo $cat->getId(); ?>"><?php echo $cat; ?></option>
 <?php } ?>
             </select>
-            <br />
+            <br/>
             <label for="titre">Titre :</label>
-            <br /><input type="text" class="form-control" id="titre" name="titre" />
-            <br />
+            <br/><input type="text" class="form-control" id="titre" name="titre" />
+            <br/>
             <label for="description">Description :</label>
-            <br />
+            <br/>
             <textarea id="description" class="form-control" name="description"></textarea>
         </div>
         <div class="form-group" style="width:70%; float:left;">
             <label for="utilisateur">Utilisateur :</label>
-            <br /><input type="text" class="form-control" id="utilisateur" name="utilisateur" readonly value="<?php echo $currentUser->getLogin(); ?>" />
-            <br />
-            <label for="datecreation">Date de création :</label>
-            <br /><input type="text" class="form-control" id="datecreation" name="datecreation" readonly value="<?php echo (new DateTime())->format('d-m-Y H:i:s'); ?>" style="width:50%;" />
-            <br />
+            <br/><input type="text" class="form-control" id="utilisateur" name="utilisateur" readonly value="<?php echo $currentUser ?>" />
+            <br/>
+            <label for="datecreation">Date de creation :</label>
+            <br/><input type="text" class="form-control" id="datecreation" name="datecreation" readonly value="<?php echo (new DateTime())->format('d-m-Y H:i:s'); ?>" style="width:50%;" />
+            <br/>
             <label for="statut">Statut :</label>
-            <br /><input type="text" class="form-control" id="statut" name="statut" readonly value="Nouveau" style="width:50%;" />
+            <br/><input type="text" class="form-control" id="statut" name="statut" readonly value="Nouveau" style="width:50%;" />
         </div>
         <div class="form-group" style="width:30%; float:right; padding-top:150px">
             <input type="submit" value="Valider" class="btn btn-default" style="width:100%;" />
-            <br /><a class="btn btn-default" href="<?php echo $config["siteUrl"]?>Tickets" style="width:100%;">Annuler</a>
+            <br/><a class="btn btn-default" href="<?php echo $config["siteUrl"]?>Tickets" style="width:100%;">Annuler</a>
         </div>
     </fieldset>
 </form>
