9539d29874f27e371166d34aa5f5f74045f94515
AntoineIcleanu <a.icleanu@laposte.net>
2015-05-31 00:30:00 +0200

    Ticket
    Tentative de résolution de problème avec la page d'ajout de ticket

--- app/views/ticket/vAdd.php
+++ app/views/ticket/vAdd.php
@@ -1,22 +1,17 @@
-<form method="post" action="Tickets/add">
+<div class="well well-lg">
+	<form method="post" action="Tickets/update">
     <fieldset>
         <legend>Creation d'un ticket</legend>
-        <div class="form-group">
-            <select name="type" class="form-control">
-                <option disabled selected>Type</option>
-                <option disabled>---</option>
-<?php foreach($ticketTypes as $type => $libelle) {  ?>
-                <option value="<?php echo $type; ?>"><?php echo $libelle; ?></option>
-<?php } ?>
-            </select>
-            <br/>
-            <select name="categorie" class="form-control">
-                <option disabled selected>Categorie</option>
-                <option disabled>---</option>
-<?php foreach($categories as $cat) {  ?>
-                    <option value="<?php echo $cat->getId(); ?>"><?php echo $cat; ?></option>
-<?php } ?>
-            </select>
+       	 	<div>       	 	
+       	 	<div>
+        	<select class="form-control" name="idCategorie">
+        		<option>Categorie</option>
+        		<?php foreach ($user as $u) {
+        		echo "<option value=".$getId().">".$u->getLibelle()."</option>";};	?>
+        	</select>
+        	</div>
+           
+        
             <br/>
             <label for="titre">Titre :</label>
             <br/><input type="text" class="form-control" id="titre" name="titre" />
@@ -24,7 +19,7 @@
             <label for="description">Description :</label>
             <br/>
             <textarea id="description" class="form-control" name="description"></textarea>
-        </div>
+        
         <div class="form-group" style="width:70%; float:left;">
             <label for="utilisateur">Utilisateur :</label>
             <br/><input type="text" class="form-control" id="utilisateur" name="utilisateur" readonly value="<?php echo $currentUser ?>" />
@@ -39,5 +34,7 @@
             <input type="submit" value="Valider" class="btn btn-default" style="width:100%;" />
             <br/><a class="btn btn-default" href="<?php echo $config["siteUrl"]?>Tickets" style="width:100%;">Annuler</a>
         </div>
+        </div>
     </fieldset>
 </form>
+</div>
