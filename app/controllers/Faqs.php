0d47ebf6e90e287d94dcae5482a07bbe543b0846
AntoineIcleanu <a.icleanu@laposte.net>
2015-05-30 23:50:05 +0200

    Début faqs
    Création de la fonction frm qui permet d'accéder à la page de création
    de la page, seulement accessible par un Admin

--- app/controllers/Faqs.php
+++ app/controllers/Faqs.php
@@ -21,4 +21,17 @@ class Faqs extends \_DefaultController {
 		$categorie=DAO::getOne("Categorie", $_POST["idCategorie"]);
 		$object->setCategorie($categorie);
 	}
+	
+	public function frm($id=NULL){
+		if(Auth::isAdmin()){
+			$object=$this->getInstance($id);
+			$cat=DAO::getAll('Categorie');
+			$date = date("d-m-y H:i:s");
+			$this->loadView("article/vAdd", array("user"=>$cat, "faq"=>$object, "date"=>$date));
+		}
+		else {
+		echo $this->messageDanger("Vous devez etre un administrateur du site pour acceder a cette apge !");
+	}
+
+}
 }
