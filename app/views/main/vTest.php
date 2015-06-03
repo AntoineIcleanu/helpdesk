bb44969f683911f43de715787143fa7424c7690a
AntoineIcleanu <a.icleanu@laposte.net>
2015-06-03 08:50:33 +0200

    Page de test
    Complet

--- app/views/main/vTest.php
+++ app/views/main/vTest.php
@@ -1,20 +1,22 @@
+<div align="center"><h3>Page d'acc&egraves aux fonctionnalit&eacutes pour tests</h3>
 	<div class="container">
 	<div class="well well-lg">
 		<fieldset>
-			<legend>Module...</legend>
-			<a class="btn btn-primary" href="#">Accueil</a>
-			<a class="btn btn-primary" href="#">Utilisateurs</a>
-			<a class="btn btn-primary" href="#">Catégories</a>
+			<legend>Module Report d'incidents</legend>
+			<a class="btn btn-primary" href="Tickets/frm">Cr&eacuteer un ticket</a>
+			<a class="btn btn-primary" href="tickets">Echanger sur un ticket...</a>
+			<a class="btn btn-primary" href="statuts">Modifier le statut d'un ticket...</a>
 		</fieldset>
 		<fieldset>
-			<legend>Module...</legend>
-				<a class="btn btn-primary" href="#">Connexion en tant qu'admin</a>
-				<a class="btn btn-primary" href="#">Connexion en tant que user</a>
-				<a class="btn btn-primary" href="#">Déconnexion</a>
+			<legend>Module base de connaissances</legend>
+				<a class="btn btn-primary" href="Faqs/frm">Cr&eacuteer un article</a>
+				<a class="btn btn-primary" href="faqs">Consulter la base</a>
+				<a class="btn btn-primary" href="faqs/frm/1">Afficher un article...</a>
 		</fieldset>
 		<fieldset>
-			<legend>Module...</legend>
-				<a class="btn btn-default" href="#">Exemple ckEditor</a>
+			<legend>Module commun</legend>
+				<a class="btn btn-default" href="defaultC/asUser">Accueil utilisateur</a>
 		</fieldset>
 		</div>
 	</div>
+</div>
